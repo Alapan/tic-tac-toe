316f8a06438338c7eba1409213131c5fac6d793e
Alapan Mukherjee <alapan.mukherjee@nightingalehealth.com>
2020-09-07 03:11:12 +0300

    WIP: Winning combinations added

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,14 +1,16 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Nought from './Nought';
 import './Box.css';
 import Cross from './Cross';
 import { StateContext } from './state';
+const isEqual = require('lodash.isequal');
 
 interface BoxProps {
-    identifier: number
+    identifier: number,
+    gameOverHandler: Function
 }
 
-const Box: React.FC<BoxProps> = ({ identifier }) => {
+const Box: React.FC<BoxProps> = ({ identifier, gameOverHandler }) => {
 
     const [boxValue, setBoxValue] = useState<string>('');
     const { state, dispatch } = useContext(StateContext);
@@ -23,6 +25,45 @@ const Box: React.FC<BoxProps> = ({ identifier }) => {
             value: identifier
         });
     }
+
+    const getNoughtOrCrossPositions = (type: String): Array<number> => {
+        const { matrix } = state;
+        const entries = matrix.filter((selection) => {
+            // @ts-ignore
+            if (selection.value === type) {
+                return selection;
+            }
+        });
+        // @ts-ignore
+        return entries.map((entry) => entry.position).sort();
+    }
+
+    const checkMatrix = () => {
+        const winningCombos = [
+            [ 1, 2, 3 ],
+            [ 1, 4, 7 ],
+            [ 2, 5, 8 ],
+            [ 3, 6, 9 ],
+            [ 4, 5, 6 ],
+            [ 7, 8, 9 ],
+            [ 1, 5, 9 ],
+            [ 3, 5, 7 ]
+        ];
+
+        // @ts-ignore
+        const noughtPositions = getNoughtOrCrossPositions('nought');
+        // @ts-ignore
+        const crossPositions = getNoughtOrCrossPositions('cross');
+        winningCombos.forEach((combo) => {
+            if (isEqual(combo, noughtPositions) || isEqual(combo, crossPositions)) {
+                gameOverHandler();
+            }
+        });
+    }
+
+    useEffect(() => {
+        checkMatrix();
+    }, [state]);
 
     return (
         <div
---
+++ src/WinningLine.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WinningLine: React.FC = () => {
+    return (
+        <div>HELLO WORLD</div>
+    );
+}
+
+export default WinningLine;
